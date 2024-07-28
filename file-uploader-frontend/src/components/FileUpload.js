d9ff5bea63682b0fccb4d9d7ebaa1cc6097f7e31
koushikadak <koushikadakka2004@gmail.com>
2024-07-28 18:22:32 +0530

    Update FileUpload.js

--- file-uploader-frontend/src/components/FileUpload.js
+++ file-uploader-frontend/src/components/FileUpload.js
@@ -179,7 +179,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 const API_URL = process.env.REACT_APP_API_URL;
-
 const FileUpload = () => {
     const [files, setFiles] = useState([]);
     const [uniqueCode, setUniqueCode] = useState('');
