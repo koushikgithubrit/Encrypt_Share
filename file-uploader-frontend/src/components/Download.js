36a91a7038166b54d1dbe28dc00eda0b16340fe5
koushikadak <koushikadakka2004@gmail.com>
2024-07-28 18:07:57 +0530

    /api add

--- file-uploader-frontend/src/components/Download.js
+++ file-uploader-frontend/src/components/Download.js
@@ -75,70 +75,70 @@
 
 
 
-import React, { useState } from 'react';
-import axios from 'axios';
-import ProgressBar from './SpinnerLoading';
-const API_URL = process.env.REACT_APP_API_URL;
+// import React, { useState } from 'react';
+// import axios from 'axios';
+// import ProgressBar from './SpinnerLoading';
+// const API_URL = process.env.REACT_APP_API_URL;
 
-const Download = () => {
-    const [uniqueCode, setUniqueCode] = useState('');
-    const [error, setError] = useState('');
-    const [isLoading, setIsLoading] = useState(false);
+// const Download = () => {
+//     const [uniqueCode, setUniqueCode] = useState('');
+//     const [error, setError] = useState('');
+//     const [isLoading, setIsLoading] = useState(false);
 
-    const handleCodeChange = (e) => {
-        setUniqueCode(e.target.value);
-    };
+//     const handleCodeChange = (e) => {
+//         setUniqueCode(e.target.value);
+//     };
 
-    const handleDownload = async (e) => {
-        e.preventDefault();
-        setIsLoading(true);
-        setError('');
-        try {
-            const response = await axios.post(`${API_URL}/download`, { uniqueCode }, {
-                responseType: 'blob'
-            });
+//     const handleDownload = async (e) => {
+//         e.preventDefault();
+//         setIsLoading(true);
+//         setError('');
+//         try {
+//             const response = await axios.post(`${API_URL}/download`, { uniqueCode }, {
+//                 responseType: 'blob'
+//             });
 
-            const url = window.URL.createObjectURL(new Blob([response.data]));
-            const link = document.createElement('a');
-            link.href = url;
-            link.setAttribute('download', `${uniqueCode}.zip`);
-            document.body.appendChild(link);
-            link.click();
-            link.remove();
+//             const url = window.URL.createObjectURL(new Blob([response.data]));
+//             const link = document.createElement('a');
+//             link.href = url;
+//             link.setAttribute('download', `${uniqueCode}.zip`);
+//             document.body.appendChild(link);
+//             link.click();
+//             link.remove();
 
-            setIsLoading(false);
-        } catch (err) {
-            setIsLoading(false);
-            setError('Invalid code');
-        }
-    };
+//             setIsLoading(false);
+//         } catch (err) {
+//             setIsLoading(false);
+//             setError('Invalid code');
+//         }
+//     };
 
-    return (
-        <div className="bg-gray-100 p-6 rounded-lg shadow-lg max-w-md mx-auto mt-10">
-            <h2 className="text-2xl font-bold text-center mb-4">Download Your Files</h2>
-            <form onSubmit={handleDownload} className="flex flex-col items-center">
-                <input 
-                    type="text" 
-                    value={uniqueCode} 
-                    onChange={handleCodeChange} 
-                    className="mb-4 p-2 border rounded w-full" 
-                    placeholder="Enter your code" 
-                    disabled={isLoading}
-                />
-                <button 
-                    type="submit" 
-                    className={`bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-purple-600 transform transition-transform duration-300 ease-in-out hover:scale-105 ${isLoading ? 'opacity-50 cursor-not-allowed' : ''}`} 
-                    disabled={isLoading}
-                >
-                    {isLoading ? <ProgressBar /> : 'Download ⇣'}
-                </button>
-            </form>
-            {error && <p className="text-red-500 mt-2">{error}</p>}
-        </div>
-    );
-};
+//     return (
+//         <div className="bg-gray-100 p-6 rounded-lg shadow-lg max-w-md mx-auto mt-10">
+//             <h2 className="text-2xl font-bold text-center mb-4">Download Your Files</h2>
+//             <form onSubmit={handleDownload} className="flex flex-col items-center">
+//                 <input 
+//                     type="text" 
+//                     value={uniqueCode} 
+//                     onChange={handleCodeChange} 
+//                     className="mb-4 p-2 border rounded w-full" 
+//                     placeholder="Enter your code" 
+//                     disabled={isLoading}
+//                 />
+//                 <button 
+//                     type="submit" 
+//                     className={`bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-purple-600 transform transition-transform duration-300 ease-in-out hover:scale-105 ${isLoading ? 'opacity-50 cursor-not-allowed' : ''}`} 
+//                     disabled={isLoading}
+//                 >
+//                     {isLoading ? <ProgressBar /> : 'Download ⇣'}
+//                 </button>
+//             </form>
+//             {error && <p className="text-red-500 mt-2">{error}</p>}
+//         </div>
+//     );
+// };
 
-export default Download;
+// export default Download;
 
 
 
@@ -147,3 +147,75 @@ export default Download;
  // "rewrites": [
   //   {"source": "/(.*)", "destination":"/"}
   // ]
+
+
+
+  import React, { useState } from 'react';
+  import axios from 'axios';
+  import ProgressBar from './SpinnerLoading';
+  const API_URL = process.env.REACT_APP_API_URL;
+  
+  const Download = () => {
+      const [uniqueCode, setUniqueCode] = useState('');
+      const [error, setError] = useState('');
+      const [isLoading, setIsLoading] = useState(false);
+  
+      const handleCodeChange = (e) => {
+          setUniqueCode(e.target.value);
+      };
+  
+      const handleDownload = async (e) => {
+          e.preventDefault();
+          setIsLoading(true);
+          setError('');
+          try {
+              const response = await axios.post(`${API_URL}/api/download`, { uniqueCode }, {
+                  responseType: 'blob'
+              });
+  
+              const url = window.URL.createObjectURL(new Blob([response.data]));
+              const link = document.createElement('a');
+              link.href = url;
+              link.setAttribute('download', `${uniqueCode}.zip`);
+              document.body.appendChild(link);
+              link.click();
+              link.remove();
+  
+              setIsLoading(false);
+          } catch (err) {
+              setIsLoading(false);
+              setError('Invalid code');
+          }
+      };
+  
+      return (
+          <div className="bg-gray-100 p-6 rounded-lg shadow-lg max-w-md mx-auto mt-10">
+              <h2 className="text-2xl font-bold text-center mb-4">Download Your Files</h2>
+              <form onSubmit={handleDownload} className="flex flex-col items-center">
+                  <input 
+                      type="text" 
+                      value={uniqueCode} 
+                      onChange={handleCodeChange} 
+                      className="mb-4 p-2 border rounded w-full" 
+                      placeholder="Enter your code" 
+                      disabled={isLoading}
+                  />
+                  <button 
+                      type="submit" 
+                      className={`bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-purple-600 transform transition-transform duration-300 ease-in-out hover:scale-105 ${isLoading ? 'opacity-50 cursor-not-allowed' : ''}`} 
+                      disabled={isLoading}
+                  >
+                      {isLoading ? <ProgressBar /> : 'Download ⇣'}
+                  </button>
+              </form>
+              {error && <p className="text-red-500 mt-2">{error}</p>}
+          </div>
+      );
+  };
+  
+  export default Download;
+  
+
+
+
+
--- server.js
+++ server.js
@@ -78,6 +78,99 @@
 
 
 
+// const express = require('express');
+// const multer = require('multer');
+// const path = require('path');
+// const { v4: uuidv4 } = require('uuid');
+// const mongoose = require('mongoose');
+// const cors = require('cors');
+// const zip = require('express-zip');
+
+// // Connect to MongoDB
+// mongoose.connect('mongodb+srv://koushikadakka2004:Emosort2004@logindatabase.f1qljez.mongodb.net/FileSeeker?retryWrites=true&w=majority');
+
+// const app = express();
+// app.use(cors());
+// app.use(express.json());
+
+// const storage = multer.diskStorage({
+//     destination: 'uploads/',
+//     filename: (req, file, cb) => {
+//         cb(null, `${Date.now()}-${file.originalname}`);
+//     }
+// });
+
+// const upload = multer({ storage });
+
+// const FileSchema = new mongoose.Schema({
+//     uniqueCode: String,
+//     files: [
+//         {
+//             path: String,
+//             originalName: String,
+//             relativePath: String,
+//         }
+//     ],
+// });
+
+// const FileModel = mongoose.model('File', FileSchema);
+
+// app.post('/upload', upload.array('files'), async (req, res) => {
+//     const files = req.files.map((file, index) => ({
+//         path: file.path,
+//         originalName: file.originalname,
+//         relativePath: req.body.paths[index],
+//     }));
+//     const uniqueCode = uuidv4();
+
+//     const newFileEntry = new FileModel({
+//         uniqueCode,
+//         files,
+//     });
+
+//     await newFileEntry.save();
+
+//     res.json({ uniqueCode });
+// });
+
+// app.post('/download', async (req, res) => {
+//     const { uniqueCode } = req.body;
+
+//     const fileEntry = await FileModel.findOne({ uniqueCode });
+
+//     if (!fileEntry) {
+//         return res.status(404).json({ error: 'Invalid code' });
+//     }
+
+//     const files = fileEntry.files.map(file => ({
+//         path: file.path,
+//         name: file.relativePath,
+//     }));
+
+//     res.zip(files, `${uniqueCode}.zip`);
+// });
+
+// // Serve static files from the React app
+// app.use(express.static(path.join(__dirname, 'client/build')));
+
+// // All other GET requests not handled before will return the React app
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+// });
+
+// const port = process.env.PORT || 5001;
+// app.listen(port, () => {
+//     console.log(`Server started on http://localhost:${port}`);
+// });
+
+
+
+
+
+
+
+
+
 const express = require('express');
 const multer = require('multer');
 const path = require('path');
@@ -86,7 +179,6 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const zip = require('express-zip');
 
-// Connect to MongoDB
 mongoose.connect('mongodb+srv://koushikadakka2004:Emosort2004@logindatabase.f1qljez.mongodb.net/FileSeeker?retryWrites=true&w=majority');
 
 const app = express();
@@ -115,7 +207,7 @@ const FileSchema = new mongoose.Schema({
 
 const FileModel = mongoose.model('File', FileSchema);
 
-app.post('/upload', upload.array('files'), async (req, res) => {
+app.post('/api/upload', upload.array('files'), async (req, res) => {
     const files = req.files.map((file, index) => ({
         path: file.path,
         originalName: file.originalname,
@@ -133,7 +225,7 @@ app.post('/upload', upload.array('files'), async (req, res) => {
     res.json({ uniqueCode });
 });
 
-app.post('/download', async (req, res) => {
+app.post('/api/download', async (req, res) => {
     const { uniqueCode } = req.body;
 
     const fileEntry = await FileModel.findOne({ uniqueCode });
@@ -151,11 +243,11 @@ app.post('/download', async (req, res) => {
 });
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'file-uploader-frontend/build')));
 
 // All other GET requests not handled before will return the React app
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'file-uploader-frontend/build', 'index.html'));
 });
 
 const port = process.env.PORT || 5001;
