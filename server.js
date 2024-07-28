6ea1dd0851c8c1d91d7c32f4e323f40202957135
koushikadak <koushikadakka2004@gmail.com>
2024-07-28 18:49:55 +0530

    from start

--- server.js
+++ server.js
@@ -78,99 +78,6 @@
 
 
 
-// const express = require('express');
-// const multer = require('multer');
-// const path = require('path');
-// const { v4: uuidv4 } = require('uuid');
-// const mongoose = require('mongoose');
-// const cors = require('cors');
-// const zip = require('express-zip');
-
-// // Connect to MongoDB
-// mongoose.connect('mongodb+srv://koushikadakka2004:Emosort2004@logindatabase.f1qljez.mongodb.net/FileSeeker?retryWrites=true&w=majority');
-
-// const app = express();
-// app.use(cors());
-// app.use(express.json());
-
-// const storage = multer.diskStorage({
-//     destination: 'uploads/',
-//     filename: (req, file, cb) => {
-//         cb(null, `${Date.now()}-${file.originalname}`);
-//     }
-// });
-
-// const upload = multer({ storage });
-
-// const FileSchema = new mongoose.Schema({
-//     uniqueCode: String,
-//     files: [
-//         {
-//             path: String,
-//             originalName: String,
-//             relativePath: String,
-//         }
-//     ],
-// });
-
-// const FileModel = mongoose.model('File', FileSchema);
-
-// app.post('/upload', upload.array('files'), async (req, res) => {
-//     const files = req.files.map((file, index) => ({
-//         path: file.path,
-//         originalName: file.originalname,
-//         relativePath: req.body.paths[index],
-//     }));
-//     const uniqueCode = uuidv4();
-
-//     const newFileEntry = new FileModel({
-//         uniqueCode,
-//         files,
-//     });
-
-//     await newFileEntry.save();
-
-//     res.json({ uniqueCode });
-// });
-
-// app.post('/download', async (req, res) => {
-//     const { uniqueCode } = req.body;
-
-//     const fileEntry = await FileModel.findOne({ uniqueCode });
-
-//     if (!fileEntry) {
-//         return res.status(404).json({ error: 'Invalid code' });
-//     }
-
-//     const files = fileEntry.files.map(file => ({
-//         path: file.path,
-//         name: file.relativePath,
-//     }));
-
-//     res.zip(files, `${uniqueCode}.zip`);
-// });
-
-// // Serve static files from the React app
-// app.use(express.static(path.join(__dirname, 'client/build')));
-
-// // All other GET requests not handled before will return the React app
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-// });
-
-// const port = process.env.PORT || 5001;
-// app.listen(port, () => {
-//     console.log(`Server started on http://localhost:${port}`);
-// });
-
-
-
-
-
-
-
-
-
 const express = require('express');
 const multer = require('multer');
 const path = require('path');
@@ -179,6 +86,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const zip = require('express-zip');
 
+// Connect to MongoDB
 mongoose.connect('mongodb+srv://koushikadakka2004:Emosort2004@logindatabase.f1qljez.mongodb.net/FileSeeker?retryWrites=true&w=majority');
 
 const app = express();
@@ -207,7 +115,7 @@ const FileSchema = new mongoose.Schema({
 
 const FileModel = mongoose.model('File', FileSchema);
 
-app.post('/api/upload', upload.array('files'), async (req, res) => {
+app.post('/upload', upload.array('files'), async (req, res) => {
     const files = req.files.map((file, index) => ({
         path: file.path,
         originalName: file.originalname,
@@ -225,7 +133,7 @@ app.post('/api/upload', upload.array('files'), async (req, res) => {
     res.json({ uniqueCode });
 });
 
-app.post('/api/download', async (req, res) => {
+app.post('/download', async (req, res) => {
     const { uniqueCode } = req.body;
 
     const fileEntry = await FileModel.findOne({ uniqueCode });
@@ -243,14 +151,106 @@ app.post('/api/download', async (req, res) => {
 });
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'file-uploader-frontend/build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 // All other GET requests not handled before will return the React app
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'file-uploader-frontend/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
 const port = process.env.PORT || 5002;
 app.listen(port, () => {
     console.log(`Server started on http://localhost:${port}`);
 });
+
+
+
+
+
+
+
+
+
+// const express = require('express');
+// const multer = require('multer');
+// const path = require('path');
+// const { v4: uuidv4 } = require('uuid');
+// const mongoose = require('mongoose');
+// const cors = require('cors');
+// const zip = require('express-zip');
+
+// mongoose.connect('mongodb+srv://koushikadakka2004:Emosort2004@logindatabase.f1qljez.mongodb.net/FileSeeker?retryWrites=true&w=majority');
+
+// const app = express();
+// app.use(cors());
+// app.use(express.json());
+
+// const storage = multer.diskStorage({
+//     destination: 'uploads/',
+//     filename: (req, file, cb) => {
+//         cb(null, `${Date.now()}-${file.originalname}`);
+//     }
+// });
+
+// const upload = multer({ storage });
+
+// const FileSchema = new mongoose.Schema({
+//     uniqueCode: String,
+//     files: [
+//         {
+//             path: String,
+//             originalName: String,
+//             relativePath: String,
+//         }
+//     ],
+// });
+
+// const FileModel = mongoose.model('File', FileSchema);
+
+// app.post('/api/upload', upload.array('files'), async (req, res) => {
+//     const files = req.files.map((file, index) => ({
+//         path: file.path,
+//         originalName: file.originalname,
+//         relativePath: req.body.paths[index],
+//     }));
+//     const uniqueCode = uuidv4();
+
+//     const newFileEntry = new FileModel({
+//         uniqueCode,
+//         files,
+//     });
+
+//     await newFileEntry.save();
+
+//     res.json({ uniqueCode });
+// });
+
+// app.post('/api/download', async (req, res) => {
+//     const { uniqueCode } = req.body;
+
+//     const fileEntry = await FileModel.findOne({ uniqueCode });
+
+//     if (!fileEntry) {
+//         return res.status(404).json({ error: 'Invalid code' });
+//     }
+
+//     const files = fileEntry.files.map(file => ({
+//         path: file.path,
+//         name: file.relativePath,
+//     }));
+
+//     res.zip(files, `${uniqueCode}.zip`);
+// });
+
+// // Serve static files from the React app
+// app.use(express.static(path.join(__dirname, 'file-uploader-frontend/build')));
+
+// // All other GET requests not handled before will return the React app
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'file-uploader-frontend/build', 'index.html'));
+// });
+
+// const port = process.env.PORT || 5002;
+// app.listen(port, () => {
+//     console.log(`Server started on http://localhost:${port}`);
+// });
