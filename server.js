cd561652dec18c9626befb7030daa5383b7b30cb
koushikadak <koushikadakka2004@gmail.com>
2024-07-31 00:16:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -159,7 +159,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'file-uploader-frontend/build', 'index.html'));
 });
 
-const port = process.env.PORT || 5050;
+const port = process.env.PORT || 10000;
 app.listen(port, () => {
     console.log(`Server started on http://localhost:${port}`);
 });
